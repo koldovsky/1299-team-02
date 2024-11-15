da5713b6b31cda80fa620b072ad1db282d391d6e
Alina-Kolomiitseva <53407424+Tilia-amurensis@users.noreply.github.com>
2024-11-16 00:29:48 +0200

    Update task-01.js

--- js.tasks/task-01.js
+++ js.tasks/task-01.js
@@ -7,3 +7,11 @@ function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   let sumGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
   return sumGoals;
   }
+//https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+function makeNegative(num) {
+  if (num < 0) {
+    return num; 
+  } else  {
+    return num * -1;
+  }
+}
