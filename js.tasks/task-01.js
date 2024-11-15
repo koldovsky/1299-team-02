6f5aee6205783004c49b1dc19ccf730a6a3af497
Alina-Kolomiitseva <53407424+Tilia-amurensis@users.noreply.github.com>
2024-11-16 00:13:42 +0200

    Update task-01.js

--- js.tasks/task-01.js
+++ js.tasks/task-01.js
@@ -2,3 +2,8 @@
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
+// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  let sumGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  return sumGoals;
+  }
