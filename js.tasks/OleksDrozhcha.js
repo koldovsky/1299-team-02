1aa3e1935df51890b367c5ac97fee626acf07e16
Oleksandr Drozhcha <o.drozhcha.nl@gmail.com>
2024-11-15 21:53:10 +0100

    Update OleksDrozhcha.js

--- js.tasks/OleksDrozhcha.js
+++ js.tasks/OleksDrozhcha.js
@@ -2,3 +2,14 @@
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
+
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+function makeNegative(num) {
+  if (num =< 0) {
+    return num;
+  } else {
+    return -num;
+  }
+}
+
+// 
