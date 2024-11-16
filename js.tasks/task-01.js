614bde6a6f839b34812fb7e6c1bb29e3d3f2e8d5
Alina Kolomiitseva <alina_kolomiitseva@epam.com>
2024-11-16 19:29:58 +0200

    Changed: task-01.js

--- js.tasks/task-01.js
+++ js.tasks/task-01.js
@@ -2,26 +2,5 @@
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
-// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
-function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-  let sumGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
-  return sumGoals;
-  }
-//https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
-function makeNegative(num) {
-  if (num < 0) {
-    return num; 
-  } else  {
-    return num * -1;
-  }
-}
-//https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
-function move (position, roll) {
-  return position + roll * 2;
-}
-//https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
-function greet (name, owner) {
-let result = (name === owner) ? 'Hello boss': "Hello guest";
-return result;
-}
+
 
