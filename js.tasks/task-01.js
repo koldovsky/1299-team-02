204646aad9910fa6084b9b7481ec5b2d0d28a33c
Alina Kolomiitseva <alina_kolomiitseva@epam.com>
2024-11-18 18:21:07 +0200

    Changed: task-01.js

--- js.tasks/task-01.js
+++ js.tasks/task-01.js
@@ -4,7 +4,7 @@
 // https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 
 function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-  let summaryGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  const summaryGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
   return summaryGoals;
 }
 
@@ -45,5 +45,5 @@ function litres(time) {
 // https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
 
 function lovefunc(flower1, flower2) {
-  return flower1 % 2 != flower2 % 2;
+  return flower1 % 2 !== flower2 % 2;
 }
