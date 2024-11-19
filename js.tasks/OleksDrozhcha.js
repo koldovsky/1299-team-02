03246182c9c09ead1d0b0d1302c8b50994d2ff42
Oleksandr Drozhcha <idroidodro@gmail.com>
2024-11-19 21:13:48 +0100

    Update OleksDrozhcha.js

--- js.tasks/OleksDrozhcha.js
+++ js.tasks/OleksDrozhcha.js
@@ -29,7 +29,7 @@ function greet(name, owner) {
 // Keep Hydrated
 // https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 function litres(time) {
-  return Math.trunc(time / 2);
+  return Math.floor(time / 2);
 }
 // Opposites Attract
 // https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
@@ -40,4 +40,31 @@ function lovefunc(flower1, flower2) {
     return false;
   }
 }
+
+-----------------------------------------------------------------------------------------
+
+// Convert a string to an array
+// https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+function stringToArray(string){
+	return string.split(" ");
+}
+
+// DNA to RNA Conversion
+// https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+function DNAtoRNA(dna) {
+  return dna.split("").map( acid => acid === "T" ? "U" : acid).join("");
+}
+
+// Find Maximum and Minimum Values of a List
+// https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+var min = function (list) {
+  list.sort((a, b) => a - b);
+  return list[0];
+};
+
+var max = function (list) {
+  list.sort((a, b) => b - a);
+  return list[0];
+};
+
 
