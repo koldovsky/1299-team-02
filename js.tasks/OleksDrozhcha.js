6a536cebbc4435958041e24f40ca7f5a89e91013
Oleksandr Drozhcha <idroidodro@gmail.com>
2024-11-19 22:36:41 +0100

    Update OleksDrozhcha.js

--- js.tasks/OleksDrozhcha.js
+++ js.tasks/OleksDrozhcha.js
@@ -81,3 +81,9 @@ function min(arr, toReturn) {
 function doubleInteger(i) {
   return i * 2;
 }
+
+// Twice as old
+// https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(dadYearsOld - sonYearsOld * 2);
+}
