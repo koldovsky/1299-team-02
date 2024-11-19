836b2d9e34139dbe858c176def2e68e3d3fb8dd7
Коваленко Олег <oleg.kovalenko@a-bank.com.ua>
2024-11-19 17:49:56 +0200

    Add solutions for task 2

--- js.tasks/OlehKovalenko.js
+++ js.tasks/OlehKovalenko.js
@@ -6,18 +6,37 @@
 // Базові задачі
 
 // https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
-function stringToArray(string){
+function stringToArray(string) {
     return string.trim().split(/\s+/);
- }
+}
 
- // https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
- function DNAtoRNA(dna) {
-    return dna.replaceAll('T','U');
+// https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+function DNAtoRNA(dna) {
+    return dna.replaceAll('T', 'U');
 }
 
 
+// https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+// Find Maximum and Minimum Values of a List
+const min = function (list) {
+    return list.sort((a, b) => b - a).pop();
+}
+
+const max = function (list) {
+    return list.sort((a, b) => a - b).pop();
+}
+
+// Поглиблені задачі
+//Smallest value of an array
+// https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+
+function min(arr, toReturn) {
+    const minValue = Math.min(...arr);
+    return toReturn === 'value' ? minValue : arr.indexOf(minValue);
+}
 
 
+// Додаткові задачки по JS
 //1.
 //Double Integer
 // https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
