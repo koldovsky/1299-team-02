a82e03473638d82594e7c32c92bc5648fc06940f
Коваленко Олег <oleg.kovalenko@a-bank.com.ua>
2024-11-19 21:29:05 +0200

    Add solutions for task 2

--- js.tasks/OlehKovalenko.js
+++ js.tasks/OlehKovalenko.js
@@ -90,5 +90,5 @@ function past(h, m, s) {
 // https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
 
 function isDivisible(n, x, y) {
-    return n % x === 0 && n % y === 0;
+    return n % x + n % y === 0;
 }
