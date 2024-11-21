25e719bcfc3efcae86c9352c0c58132e59561894
Oleksandr Drozhcha <idroidodro@gmail.com>
2024-11-21 23:08:53 +0100

    Update OleksDrozhcha.js

--- js.tasks/OleksDrozhcha.js
+++ js.tasks/OleksDrozhcha.js
@@ -148,4 +148,30 @@ const buildFun = (n) => {
   return result;
 };
 
+// Fun with ES6 Classes #2 - Animals and Inheritance
+// https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
+  }
+}
 
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, "cat", status);
+  }
+  introduce() {
+    return super.introduce() + "  Meow meow!";
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, "dog", status);
+    this.master = master;
+  }
+
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
