5efd9e74a8728e4bd9cedb1183c455dd065ff9f3
Skiper29 <61919416+Skiper29@users.noreply.github.com>
2024-11-21 18:46:09 +0200

    Add solutions for third tasks to VitaliiDemiv.js

--- js.tasks/VitaliiDemiv.js
+++ js.tasks/VitaliiDemiv.js
@@ -34,6 +34,7 @@ function lovefunc(flower1, flower2) {
   return flower1 % 2 !== flower2 % 2;
 }
 
+// ----------------------------------------------------------------------------------------
 // Tasks 2
 
 // Convert a string to an array
@@ -99,4 +100,81 @@ function past(h, m, s) {
 // https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
 function isDivisible(n, x, y) {
   return n % x === 0 && n % y === 0;
+}
+
+// ----------------------------------------------------------------------------------------
+// Tasks 3
+
+// Geometry Basics: Circle Circumference in 2D
+//https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+function circleCircumference(circle) {
+  return 2 * Math.PI * circle.radius;
+}
+
+// Training JS #12: loop statement --for..in and for..of
+// https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+function giveMeFive(obj) {
+  const result = [];
+  for (let key in obj) {
+    if (key.length === 5) result.push(key);
+    if (obj[key].length === 5) result.push(obj[key]);
+  }
+  return result;
+}
+
+// Understanding closures - the basics
+//https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+
+function buildFun(n) {
+  const res = [];
+
+  for (let i = 0; i < n; i++) {
+    res.push(function () {
+      return i;
+    });
+  }
+  return res;
+}
+
+// Fun with ES6 Classes #2 - Animals and Inheritance
+//https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+
+class Animal {
+  constructor(name, age, legs, species, status) {
+    this.name = name;
+    this.age = age;
+    this.legs = legs;
+    this.species = species;
+    this.status = status;
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+  }
+}
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, "cat", status);
+  }
+
+  introduce() {
+    return super.introduce() + "  Meow meow!";
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, "dog", status);
+    this.master = master;
+  }
+
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
 }
