2ca34f72cc5460fd34ca3e358aa034149e577ced
Oleksandr Drozhcha <idroidodro@gmail.com>
2024-11-22 15:42:44 +0100

    Update task-03.js

--- js.tasks/task-03.js
+++ js.tasks/task-03.js
@@ -8,23 +8,73 @@ function circleCircumference(circle) {
 //2
 //https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
 //Training JS #12: loop statement --for..in and for..of
-function giveMeFive(obj){
-  let resultArr = [];
-  for(const value in obj){
-    if(value.length === 5){
-      resultArr.push(value);
+function giveMeFive(obj) {
+  const newArr = [];
+  for (let key in obj) {
+    if (key.length === 5) {
+      newArr.push(key);
     }
-    if(obj[value].length === 5){
-      resultArr.push(obj[value]);
+    if (typeof obj[key] === "string" && obj[key].length === 5) {
+      newArr.push(obj[key]);
     }
   }
-  return resultArr;
+  return newArr;
 }
 
 //3
 //https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
 //Understanding closures - the basics
+function buildFun(n) {
+  const res = [];
+
+  for (let i = 0; i < n; i++) {
+    res.push(function () {
+      return i;
+    });
+  }
+  return res;
+}
 
 //4
 //https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
 //Fun with ES6 Classes #2 - Animals and Inheritance
+
+class Animal {
+  constructor(name, age, legs, species, status) {
+    this.name = name;
+    this.age = age;
+    this.legs = legs;
+    this.species = species;
+    this.status = status;
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+  }
+}
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status); // No legs
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, "cat", status); // Legs and species are hardcoded
+  }
+
+  introduce() {
+    return `${super.introduce()}  Meow meow!`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, "dog", status); // Legs and species are hardcoded
+    this.master = master;
+  }
+
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
