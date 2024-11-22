cb516856bde7be37e14a098855f07cfc2e9285ab
Anton Kostin <113688793+AntonKostin@users.noreply.github.com>
2024-11-22 14:32:35 +0200

    Update task-03.js

--- js.tasks/task-03.js
+++ js.tasks/task-03.js
@@ -1,1 +1,19 @@
+//1
+//https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+//Geometry Basics: Circle Circumference in 2D
+function circleCircumference(circle) {
+  return circle.radius * Math.PI * 2;
+}
 
+//2
+//https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+//Training JS #12: loop statement --for..in and for..of
+
+
+//3
+//https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+//Understanding closures - the basics
+
+//4
+//https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+//Fun with ES6 Classes #2 - Animals and Inheritance
