075f6102f1c49993c356aaaca1fe3276404b753e
Alina Kolomiitseva <alina_kolomiitseva@epam.com>
2024-11-22 12:16:17 +0200

    Added: tasks for Alina.js

--- js.tasks/Alina.js
+++ js.tasks/Alina.js
@@ -1,6 +1,58 @@
+//Results for tasks-3
+//https://www.codewars.com/kata/58e43389acfd3e81d5000a88/train/javascript
+function circleCircumference(circle) {
+  return 2 * Math.PI * circle.radius;
+  }
+//https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+function giveMeFive(obj){
+  const newArr = [];
+  for(let key in obj) {
+  if (key.length === 5) {
+  newArr.push(key);
+  }
+  if (obj[key].length === 5)
+  newArr.push(obj[key]);
+  }
+    return newArr;
+  }
+
+  class Shark extends Animal {
+    constructor(name, age, status) {
+      // Call the parent constructor with hardcoded values for legs and species
+      super(name, age, 0, "shark", status);
+    }
+  
+  }
+  class Cat extends Animal {
+    constructor(name, age, status) {
+      // Call the parent constructor
+      super(name, age, 4, "cat", status); // Legs and species are hardcoded
+    }
+  
+    // Override the introduce method
+    introduce() {
+      return `${super.introduce()}  Meow meow!`;
+    }
+  }
+  
+  // Dog class inheriting Animal
+  class Dog extends Animal {
+    constructor(name, age, status, master) {
+      // Call the parent constructor
+      super(name, age, 4, "dog", status); // Legs and species are hardcoded
+      this.master = master; // Assign master
+    }
+  
+    // Override the introduce method
+    greetMaster() {
+      return `Hello ${this.master}`;
+    }
+  }
+
+//
 //Results for tasks-2
 //https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
-function stringToArray(string){
+function stringToArray(string) {
   return string.split(" ");
 }
 //https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
@@ -8,30 +60,30 @@ function DNAtoRNA(dna) {
   let arrDna = dna.split("");
   for (let i = 0; i < arrDna.length; i++) {
     if (arrDna[i] === "T") {
-      arrDna[i] = "U"; 
+      arrDna[i] = "U";
     }
   }
-  return arrDna.join(""); 
+  return arrDna.join("");
 }
 
 //https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
-const min = function(list){
-  return list.reduce((a, b) => a < b ? a : b);
- }
- 
- const max = function(list){
-   return list.reduce((a, b) => a > b ? a : b);
- }
+const min = function (list) {
+  return list.reduce((a, b) => (a < b ? a : b));
+};
 
- //https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
- function min(arr, toReturn) { 
+const max = function (list) {
+  return list.reduce((a, b) => (a > b ? a : b));
+};
+
+//https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+function min(arr, toReturn) {
   const minItem = Math.min(...arr);
-if (toReturn === "value") {
-  return minItem;
-} else {
-return arr.indexOf(minItem);
+  if (toReturn === "value") {
+    return minItem;
+  } else {
+    return arr.indexOf(minItem);
+  }
 }
-}  
 
 //Results for tasks-1
 
@@ -54,7 +106,7 @@ function move(position, roll) {
 }
 //https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 function greet(name, owner) {
-  return name === owner ? "Hello boss": "Hello guest";
+  return name === owner ? "Hello boss" : "Hello guest";
 }
 //https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 function litres(time) {
@@ -64,8 +116,7 @@ function litres(time) {
 function lovefunc(flower1, flower2) {
   if (flower1 % 2 !== flower2 % 2) {
     return true;
-  }
-  else {
+  } else {
     return false;
   }
 }
