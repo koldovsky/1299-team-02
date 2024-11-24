e23550ad7b8f5173b018f2aa6f742d581de5c33c
Коваленко Олег <oleg.kovalenko@a-bank.com.ua>
2024-11-24 13:50:41 +0200

    add index.diving-packages.js

--- js/index.diving-package.js
+++ js/index.diving-package.js
@@ -1,19 +1,19 @@
 const packages = [
     {
         image: "img/diving-packages/diving1.png",
-        service: "3 Days - 9 Dives",
+        name: "3 Days - 9 Dives",
         price: 300,
         description: "Make the most of your diving experience in Phuket. We will go diving 3 times a day, 3 days in a row, in different nearby places."
     },
     {  
         image: "img/diving-packages/diving2.png", 
-        service: "Night Dives",
+        name: "Night Dives",
         price: 300,
         description: "Night diving is something you should try. The underwater world is spectacular at night! The offer is only for AOWD divers."
     },
     {
         image: "img/diving-packages/diving3.png", 
-        service: "3 Caves - 3 Days",
+        name: "3 Caves - 3 Days",
         price: 250,
         description: " For Advanced Divers Only. We go diving to undisclosed caves in the Phuket surroundings. One day - one cave!"
     }
@@ -26,8 +26,8 @@ function renderDivingPackages(packages){
     for (const service of packages){
         packagesHtml += `
          <article>
-      <img src="${service.image}" alt="3 Days - 9 Dives service" />
-      <p class="diving-packages__content--service">${service.service}</p>
+      <img src="${service.image}" alt="${service.name}" />
+      <p class="diving-packages__content--service">${service.name}</p>
       <p class="diving-packages__content--price">from $ ${service.price}</p>
       <p class="diving-packages__content--description">${service.description}</p>
       <a class="diving-packages__content--more" href="#">LEARN MORE</a>
