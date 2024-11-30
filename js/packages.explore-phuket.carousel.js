ac10ef3e1545752dd9c826fca87cf69c6a0a0fe8
Oleksandr Drozhcha <idroidodro@gmail.com>
2024-11-30 18:20:53 +0100

    updated: explore-phuket carousel with autorun function

--- js/packages.explore-phuket.carousel.js
+++ js/packages.explore-phuket.carousel.js
@@ -1,7 +1,58 @@
+let nextDom = document.getElementById("arrows__next");
+let prevDom = document.getElementById("arrows__prev");
+let carouselDom = document.querySelector(".explore__carousel");
+let listItemsDom = document.querySelector(
+  ".explore__carousel .explore__carousel-list"
+);
+let thumbnailDom = document.querySelector(
+  ".explore__carousel .explore__carousel-thumbnail"
+);
 
+nextDom.onclick = function () {
+  showSlider("arrows__next");
+};
 
+prevDom.onclick = function () {
+  showSlider("arrows__prev");
+};
 
+let timeRunning = 2001;
+let runTimeOut;
+const timeOutNext = 5000;
+let runAutoRun = (runAutoRun = setInterval(() => {
+  nextDom.click();
+}, timeOutNext));
 
+function showSlider(type) {
+  let itemsSlider = document.querySelectorAll(
+    ".explore__carousel .explore__carousel-list .carousel__item"
+  );
+  let itemsThumbnail = document.querySelectorAll(
+    ".explore__carousel .explore__carousel-thumbnail .carousel__item"
+  );
+
+  if (type === "arrows__next") {
+    listItemsDom.appendChild(itemsSlider[0]);
+    thumbnailDom.appendChild(itemsThumbnail[0]);
+    carouselDom.classList.add("arrows__next");
+  } else {
+    const positionLastItem = itemsSlider.length - 1;
+    listItemsDom.prepend(itemsSlider[positionLastItem]);
+    thumbnailDom.prepend(itemsThumbnail[positionLastItem]);
+    carouselDom.classList.add("arrows__prev");
+  }
+
+  clearTimeout(runTimeOut);
+  runTimeOut = setTimeout(() => {
+    carouselDom.classList.remove("arrows__next");
+    carouselDom.classList.remove("arrows__prev");
+  }, timeRunning);
+
+  clearTimeout(runAutoRun);
+  runAutoRun = setInterval(() => {
+    nextDom.click();
+  }, timeOutNext);
+}
 
 // const images = document.querySelectorAll(".carousel__img");
 // let currentIndex = 0;
@@ -20,7 +71,6 @@
 // images[0].classList.add("active");
 
 // setInterval(moveCarousel, 3000);
-
 
 // function moveCarousel() {
 //     const gallery = document.querySelector(".explore__gallery");
