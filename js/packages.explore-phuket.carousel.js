c77114d7219a8a1f1e7a4a7daf6b894d4984561b
Oleksandr Drozhcha <idroidodro@gmail.com>
2024-11-30 12:54:13 +0100

    explore-phuket carousel in progress

--- js/packages.explore-phuket.carousel.js
+++ js/packages.explore-phuket.carousel.js
@@ -1,17 +1,35 @@
-const images = document.querySelectorAll(".carousel__img");
-let currentIndex = 0;
 
-function moveCarousel() {
-  const currentImg = images[currentIndex];
-  const nextIndex = (currentIndex + 1) % images.length;
-  const nextImg = images[nextIndex];
-  currentImg.classList.remove("active");
-  currentImg.classList.add("prev");
-  nextImg.classList.add("active");
-  setTimeout(() => currentImg.classList.remove("prev"), 500);
-  currentIndex = nextIndex;
-}
 
-images[0].classList.add("active");
 
-setInterval(moveCarousel, 2000);
+
+
+// const images = document.querySelectorAll(".carousel__img");
+// let currentIndex = 0;
+
+// function moveCarousel() {
+//   const currentImg = images[currentIndex];
+//   const nextIndex = (currentIndex + 1) % images.length;
+//   const nextImg = images[nextIndex];
+//   currentImg.classList.remove("active");
+//   currentImg.classList.add("prev");
+//   nextImg.classList.add("active");
+//   setTimeout(() => currentImg.classList.remove("prev"), 500);
+//   currentIndex = nextIndex;
+// }
+
+// images[0].classList.add("active");
+
+// setInterval(moveCarousel, 3000);
+
+
+// function moveCarousel() {
+//     const gallery = document.querySelector(".explore__gallery");
+//     const items = gallery.querySelectorAll(".carousel__img");
+
+//     if (items.length > 1) {
+//         const firstImg = items[0];
+//         gallery.appendChild(firstImg);
+//     }
+// }
+
+// setInterval(moveCarousel, 3000);
